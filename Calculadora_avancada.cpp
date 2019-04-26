4a8abf4c98f1a0151d598ba6685daed76371e309
Mondchef <gabrielfernandes.b.c@gmail.com>
2019-04-25 23:27:41 -0300

    Adicionando o main ao programa

---
+++ Calculadora_avancada.cpp
@@ -0,0 +1,37 @@
+main(){
+	setlocale(LC_ALL,"portuguese");
+	
+	int saida1;
+	char saida[50];
+	double log, a, base;
+	float n1,n2,r;
+	float x = 0;
+	int cont = 0;
+	char operador,zero[] ="0"; 
+	
+	while(x==0){
+		n1=0;
+		n2=0;
+		
+		printf("********************************************************************************\n");
+		printf("****ATENÇÃO SE A OPERAÇÃO FOR INVÁLIDA O PROGRAMA IRÁ FECHAR AUTOMATICAMENTE****\n");
+		printf("********************************************************************************");
+		if(cont!=0){
+			printf("\n\nO RESULTADO DA OPERAÇÃO ANTERIOR FOI: %.2lf\n", r);
+		}
+		printf("\n\nCALCULADORA\nComandos:\nSoma = '+'\nSubtração = '-'\nMultiplicação = '*' \nDivisão = '/'\nPorcentagem = '%%' \nExponencial = '^'\nLogaritmo = 'l'\nRaíz quadrada = 'v'");
+		printf("\n\nDigite o primeiro número, a operação e logo depois o segundo número\n\n");	
+		scanf("%f %c %f", &n1, &operador, &n2);
+		
+		printf("\nSe deseja continuar na calculadora digite 0\nSe não qualquer número:\n");
+    	fflush(stdin);
+		gets(saida);
+		if(strcmp(saida,zero)){
+    		exit(1);
+		}
+		else{
+    	system("cls");
+    	cont++;
+    }
+    }
+}
